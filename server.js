24a1ab17fed6171db94a3b37c87879660d9be9ce
AdamCooke00 <AdamCooke00@gmail.com>
2023-01-28 14:32:50 -0500

    add folder structure and firebase auth. Move css and html into public files. Add auth "middleware" to checkout. Add temperary CSS for development sanity. Add body-parser

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const bodyParser = require("body-parser");
+const {auth} = require('./firebase.js');
 
+const  { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut   } = require ('firebase/auth');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -17,9 +23,13 @@ app.get('/claim-ada', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'claim-ada.html'));
 });
 
-// app.get('/checkout', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
-// });
+app.get('/checkout', (req, res) => {
+  if(!auth.currentUser){
+    res.redirect('/signin')
+  } else{
+    res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
+  }
+});
 
 app.get('/successful-payment', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'successful-payment.html'));
@@ -29,21 +39,82 @@ app.get('/cancel-payment', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'cancel-payment.html'));
 });
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1MV5jkK4cnhnlETsUTpP3RMJ',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/successful-payment.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel-payment.html`,
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signin.html'));
+});
+
+
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user.uid)
+
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+    // ..
   });
 
-  res.redirect(303, session.url);
+});
+
+app.post('/signin', async (req, res) => {
+  if(!auth.currentUser){
+    const { email, password } = req.body;
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user.uid)
+      res.redirect("/")
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+    });
+  }
+});
+
+app.get('/signout', async (req, res) => {
+  signOut(auth).then(() => {
+    console.log("Sign out success")
+  }).catch((error) => {
+    console.log(errorMessage)
+  });
+
+  res.redirect(303, '/');
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  if(!auth.currentUser){
+    res.redirect('/signin')
+  } else {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1MV5jkK4cnhnlETsUTpP3RMJ',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/successful-payment.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel-payment.html`,
+    });
+  
+    res.redirect(303, session.url);
+  }
+  
 });
 
 const PORT = process.env.PORT || 4242;
